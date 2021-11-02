c7284031698e39507a89e0ad0e3b0e2a87491477
rgocella <rgocella@smartnet.com.ar>
2021-11-02 18:12:31 -0300

    feat:roles-navbar-sidebar

---
+++ src/pages/roles/create/index.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'solid-js';
+import RoleCreate from '../../../templates/roles/RoleCreate';
+
+const IndexPage: Component = () => {
+    return (
+        <>
+
+            <RoleCreate />
+
+        </>
+    );
+};
+
+export default IndexPage;
