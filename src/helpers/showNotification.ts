52c096b2f4a18862cdfeb8a44e51155d472198f7
lalolipc <mardelplatarocio@gmail.com>
2022-02-01 11:11:04 -0300

    feat: notification change password

---
+++ src/helpers/showNotification.ts
@@ -0,0 +1,15 @@
+// import { showGeneralNotification } from './actions';
+// import { notification, notificationTypes } from '../../entities';
+
+import { createSignal } from 'solid-js';
+import notify from './notif';
+const [ showNotification, setShowNotification ] = createSignal();
+const [ showNotify, setShowNotify ] = createSignal( false );
+
+export const showSuccessNotification = ( message:string ) =>
+    setShowNotification( notify( {  msg:message, in: 'row success intro', duration: 2.9, progressbar: 'notif-progress-bar', out: 'outro' } ) );
+
+export const showErrorNotification = ( message:string ) =>
+    setShowNotification( notify( { msg:message, in: 'row error intro', duration: 2.9, progressbar: 'notif-progress-bar', out: 'outro' } ) );
+
+export {  showNotification, setShowNotification, showNotify, setShowNotify };
--- src/templates/users/UserChangePassword.tsx
+++ src/templates/users/UserChangePassword.tsx
@@ -1,12 +1,9 @@
-// import ButtonClose from "../../molecules/ButtonClose";
-// import ButtonConfirm from "../../molecules/ButtonConfirm";
 import Button from '../../atoms/Button';
 import { Component } from 'solid-js';
 import Input from '../../atoms/Input';
 import Title from '../../atoms/Title';
 import { Form } from 'solid-js-form';
 import ChangePasswordSchema from '../../SchemaValidations/ChangePasswordSchema';
-import AuthRepository from '../../repositories/AuthRepository';
 
 interface UserChangePasswordTemplateProps
 {
@@ -17,62 +14,70 @@ interface UserChangePasswordTemplateProps
 const UserChangePassword: Component<UserChangePasswordTemplateProps> =  ( props ) =>
 {
     return (
-        <section class="text-gray-500 body-font bg-gray-900 w-full md:container mx-auto px-3">
-            <div class="mb-2 ">
-                <Title class="text-3xl font-bold sm:px-0 md:px-18 lg:px-14" titleType="h1">
-          Change Password
-                </Title>
-            </div>
+        <section class="dg-main-bg h-screen">
+            <div class="dg-full-center-flex">
+                <div class="dg-rounded-small-box py-5 ">
+                    <div class="text-gray-500 body-font  w-full  mx-auto px-3 pt-2 md:pt-10">
+                        <div class="mb-2 ">
+                            <Title class="text-3xl font-bold sm:px-0 md:px-18 lg:px-14" titleType="h1">
+                                Change Password
+                            </Title>
+                        </div>
 
-            <Form
-                initialValues={{
-                    confirmationToken: props.confirmationToken,
-                    password: '',
-                    passwordConfirmation: ''
-                }}
-                validation={ChangePasswordSchema}
-                onSubmit={async ( form ) =>
-                {
-                    props.changePasswordAction( form.values );
-                }}
+                        <Form
+                            initialValues={{
+                                confirmationToken: props.confirmationToken,
+                                password: '',
+                                passwordConfirmation: ''
+                            }}
+                            validation={ChangePasswordSchema}
+                            onSubmit={async ( form ) =>
+                            {
+                                props.changePasswordAction( form.values );
+                            }}
 
-            >
+                        >
 
-                <div class="sm:px-0 md:px-16 lg:px-14 flex flex-wrap mb-6 text-sm">
-                    <div class="w-full px-2 mb-5">
+                            <div class="sm:px-0 md:px-16 lg:px-14 flex flex-wrap mb-6 text-sm">
+                                <div class="w-full px-2 mb-5">
 
-                        <Input
-                            name="password"
-                            type="password"
-                            id="password"
-                            class="w-full bg-gray-800 border rounded-full border-gray-700 text-white focus:outline-none focus:border-indigo-500 text-base hover:border-grey px-2 py-3 h-10 shadow font-bold"
-                            placeholder="Enter Password"
-                            labelClass="dg-form-label"
-                            labelName="Password"
-                        />
+                                    <Input
+                                        name="password"
+                                        type="password"
+                                        id="password"
+                                        class="dg-form-field-full font-extrabold pl-5  focus:border-indigo-500 text-base hover:border-grey "
+                                        placeholder="Enter Password"
+                                        labelClass="dg-form-label"
+                                        labelName="Password"
+                                    />
+                                </div>
+                                <div class="w-full px-2 mb-5">
+
+                                    <Input
+                                        name="passwordConfirmation"
+                                        type="password"
+                                        id="passwordConfirmation"
+                                        class="dg-form-field-full font-extrabold pl-5  focus:border-indigo-500 text-base hover:border-grey "
+                                        placeholder="Repeat Password"
+                                        labelClass="dg-form-label"
+                                        labelName="Confirm Password"
+                                    />
+                                </div>
+                                <div class="w-full  flex flex-row-reverse ">
+                                    <Button  class="dg-main-button" type="submit">
+                                        Save
+                                    </Button>
+                                    <Button  class="dg-secondary-button mr-1" onClick={props.changePasswordAction()}>
+                                        Close
+                                    </Button>
+                                </div>
+                            </div>
+                        </Form>
+
                     </div>
-                    <div class="w-full px-2 mb-5">
 
-                        <Input
-                            name="passwordConfirmation"
-                            type="password"
-                            id="passwordConfirmation"
-                            class="w-full bg-gray-800 border rounded-full border-gray-700 text-white focus:outline-none focus:border-indigo-500 text-base  hover:border-grey px-2 py-3 h-10 shadow font-bold"
-                            placeholder="Repeat Password"
-                            labelClass="dg-form-label"
-                            labelName="Confirm Password"
-                        />
-                    </div>
-                    <div class="w-full mt-5 flex flex-row-reverse">
-                        <Button  class="dg-main-button" type="submit">
-                          Save
-                        </Button>
-                        <Button  class="dg-secondary-button" onClick={() => true}>
-                          Close
-                        </Button>
-                    </div>
                 </div>
-            </Form>
+            </div>
         </section>
     );
 };
