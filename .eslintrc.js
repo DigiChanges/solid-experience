db8de560325ea0e4e4a9bce3a6f80a3d2941c937
Leonardo Casales <leonardo@smartnet.com.ar>
2022-02-27 21:38:41 -0300

    feat: eslint rules added from git repository

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+module.exports = {
+    extends: [
+        require.resolve( 'solidjs-eslint-config' ),
+    ],
+} ;
--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App ()
 {
     const Routes = useRoutes( dashRoutes );
 
-    return <Routes/>;
+    return <Routes />;
 }
 
 export default App;
