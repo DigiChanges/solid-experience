d70efda5cb27e249455587198892d3e652450dd5
Leonardo Casales <leonardo@smartnet.com.ar>
2022-03-08 17:59:16 -0300

    style: fix remove modal hscroll

--- src/molecules/Modal.tsx
+++ src/molecules/Modal.tsx
@@ -8,7 +8,7 @@ interface ModalTemplateProps
 
 const Modal: Component<ModalTemplateProps> = ( props ) => (
     <Portal>
-        <div class="absolute top-0 h-screen w-screen items-center z-20 bg-transparent-black" >
+        <div class="absolute top-0 h-screen w-full items-center z-20 bg-transparent-black" >
             {props.children}
         </div>
     </Portal>
