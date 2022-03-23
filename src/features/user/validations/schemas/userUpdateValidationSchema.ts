63085ade4e780eeac20a4abfa94acce343bd8123
Leonardo Casales <leonardo@smartnet.com.ar>
2022-03-23 16:33:35 -0300

    refactor: user shared validations

--- src/features/user/validations/schemas/userUpdateValidationSchema.ts
+++ src/features/user/validations/schemas/userUpdateValidationSchema.ts
@@ -1,40 +1,7 @@
-import * as Yup from 'yup';
+import userValidationSchema from './userValidationSchema';
 
 const userUpdateValidationSchema = ( t: any ) => ( {
-    firstName: Yup.string()
-        .min( 2, t( 'av_too_short' ) )
-        .max( 50, t( 'av_too_long' ) )
-        .required(  t( 'av_required' ) ),
-    lastName: Yup.string()
-        .min( 2, t( 'av_too_short' ) )
-        .max( 50, t( 'av_too_long' ) )
-        .required(  t( 'av_required' ) ),
-    email: Yup.string()
-        .email( 'Invalid email' )
-        .required(  t( 'av_required' ) ),
-    gender: Yup.string()
-        .oneOf( [ 'male', 'fame', 'other' ], t( 'av_required' ) )
-        .required(  t( 'av_required' ) ),
-    country: Yup.object()
-        .required(  t( 'av_required' ) ),
-    birthday: Yup.string()
-        .required(  t( 'av_required' ) ),
-    phone: Yup.string()
-        .max( 20,  t( 'av_too_long' ) )
-        .required(  t( 'av_required' )  ),
-    documentType: Yup.object()
-        .required(  t( 'av_required' ) ),
-    documentNumber: Yup.string()
-        .required(  t( 'av_required' ) ),
-    address: Yup.string()
-        .required(  t( 'av_required' ) ),
-    enable: Yup.object()
-        .required(  t( 'av_required' ) ),
-    roles: Yup.array()
-        .min( 1, t( 'av_one_item' )  )
-        .required(  t( 'av_required' ) ),
-    permissions: Yup.array()
-        .min( 1, t( 'av_one_item' )  ),
+    ...userValidationSchema( t ),
 } );
 
 export default userUpdateValidationSchema;
