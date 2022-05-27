2a06cdbf6b2ff6783787c950b2b50ed98b22dffc
lalolipc <mardelplatarocio@gmail.com>
2022-05-27 12:46:51 -0300

    feat: take offaccount js

--- json-server/account.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = ( req, res, next ) =>
-{
-    if ( req.url === '/users' && req.method === 'POST' )
-    {
-        req.method = 'GET';
-    }
-    next();
-};
