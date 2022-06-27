3402ed0a511b5c2b82312b7a70638ea0bc0e581e
Leonardo Casales <leonardo@smartnet.com.ar>
2022-06-26 22:19:46 -0300

    feat: create and remove refresh token on login and logout in json server

--- json-server/login.js
+++ json-server/login.js
@@ -1,8 +1,41 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require( 'fs' );
+
 module.exports = ( req, res, next ) =>
 {
     if ( req.url === '/super-admin-login' && req.method === 'POST' )
     {
+        const refreshToken = {
+            user: {
+                id: '922465bc-db4e-4794-b25c-9311ce124e00',
+                firstName: 'Super',
+                lastName: 'Admin',
+                email: 'superadmin@node.com',
+                enable: true,
+                permissions: [],
+                roles: [],
+                isSumerAdmin: true,
+                createdAt: 1651255968,
+                updatedAt: 1651255968,
+            },
+            expires: 1689176366,
+            token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpZCI6IjIxNDJiMjMzLTEwZDUtNDIyZS04OTRmLTc3MzUwMzA2ZWI5NSIsImlzcyI6InNtYXJ0bmV0IiwiYXVkIjoic21hcnRuZXQuY29tIiwic3ViIjoic3VwZXJhZG1pbkBub2RlLmNvbSIsImlhdCI6MTY4OTE3NjM2NiwiZXhwIjoxNjg5MTc2MzY2LCJ1c2VySWQiOiI5MjI0NjViYy1kYjRlLTQ3OTQtYjI1Yy05MzExY2UxMjRlMDAiLCJlbWFpbCI6InN1cGVyYWRtaW5Abm9kZS5jb20iLCJ0ZW5hbnQiOiJwdWJsaWMifQ.TpYXC1KqDO8XjbAqlP1joX0ye1Drsh2NMnOCS4Cn8bC3mQ_iRVl6tpTtK1WU_WcQB4c0vrRiULbDz6CHyDdAIQ',
+        };
+
+        fs.readFile( './db.json', 'utf-8', ( err, jsonData ) =>
+        {
+            if ( err ) { return console.error( 'Error to read db.json' ); }
+            const data = JSON.parse( jsonData );
+
+            data['refresh-token'] = refreshToken;
+
+            fs.writeFile( './db.json', JSON.stringify( data, null, 2 ), ( err ) =>
+            {
+                if ( err ) { return console.error( 'Error to save refresh-token' ); }
+            } );
+        } );
+
         req.method = 'GET';
     }
     next();
--- json-server/logout.js
+++ json-server/logout.js
@@ -1,9 +1,24 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require( 'fs' );
+
 // eslint-disable-next-line no-undef
 module.exports = ( req, res, next ) =>
 {
     if ( req.url === '/logout' && req.method === 'POST' )
     {
-        req.method = 'GET';
+        fs.readFile( './db.json', 'utf-8', ( err, jsonData ) =>
+        {
+            const data = JSON.parse( jsonData );
+
+            delete data['refresh-token'];
+
+            const dataWithoutRefreshToken = JSON.stringify( data, null, 2 );
+
+            fs.writeFileSync( './db.json', dataWithoutRefreshToken );
+
+            req.method = 'GET';
+        } );
     }
     next();
 };
