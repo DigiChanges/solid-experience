659a4af7d1e69bf3467716b52726d277a7fd6942
Leonardo Casales <leonardo@smartnet.com.ar>
2022-07-01 21:51:00 -0300

    refactor: jsonServer jsonRepository, login, logout

---
+++ json-server/jsonRepository.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require( 'fs' );
+
+function getDbData ( cb )
+{
+    fs.readFile( './db.json', 'utf-8', ( err, jsonData ) =>
+    {
+        if ( err ) { return cb( err ); }
+        const data = JSON.parse( jsonData );
+        cb( null, data );
+    } );
+}
+function saveDbData ( data, cb )
+{
+    fs.writeFile( './db.json', JSON.stringify( data, null, 2 ), cb );
+}
+
+const jsonRepository = {
+    saveRefreshToken ( refreshToken )
+    {
+        getDbData( ( err, data ) =>
+        {
+            if ( err ) { return console.error( 'Error to read db.json' ); }
+
+            data['refresh-token'] = refreshToken;
+
+            saveDbData( data, ( err ) =>
+            {
+                if ( err ) { return console.error( 'Error to save refresh-token' ); }
+            } );
+        } );
+    },
+    removeRefreshToken ( cb )
+    {
+        getDbData( ( err, data ) =>
+        {
+            delete data['refresh-token'];
+
+            if ( err ) { return console.error( 'Error to read db.json' ); }
+
+            saveDbData( data, ( err ) =>
+            {
+                if ( err ) { return console.error( 'Error to save refresh-token' ); }
+                cb();
+            } );
+        } );
+    },
+};
+
+module.exports = jsonRepository;
--- json-server/logout.js
+++ json-server/logout.js
@@ -1,24 +1,20 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const fs = require( 'fs' );
+const jsonRepository = require( './jsonRepository' );
 
 // eslint-disable-next-line no-undef
 module.exports = ( req, res, next ) =>
 {
     if ( req.url === '/logout' && req.method === 'POST' )
     {
-        fs.readFile( './db.json', 'utf-8', ( err, jsonData ) =>
+        req.method = 'GET';
+        jsonRepository.removeRefreshToken( () =>
         {
-            const data = JSON.parse( jsonData );
-
-            delete data['refresh-token'];
-
-            const dataWithoutRefreshToken = JSON.stringify( data, null, 2 );
-
-            fs.writeFileSync( './db.json', dataWithoutRefreshToken );
-
-            req.method = 'GET';
+            next();
         } );
     }
-    next();
+    else
+    {
+        next();
+    }
 };
