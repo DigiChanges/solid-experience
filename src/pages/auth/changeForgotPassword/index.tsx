ba3d1fb387fd9e5be5643104c578bfa763f440a2
lalolipc <mardelplatarocio@gmail.com>
2022-07-06 16:13:56 -0300

    feat:forgot password with gral module and alerts hope

--- src/pages/auth/changeForgotPassword/index.tsx
+++ src/pages/auth/changeForgotPassword/index.tsx
@@ -2,23 +2,18 @@ import { useSearchParams } from 'solid-app-router';
 import { Component } from 'solid-js';
 import ChangeForgotPassword from '../../../features/auth/forgotPassword/templates/ChangeForgotPassword/ChangeForgotPassword';
 import AuthRepository from '../../../features/auth/repositories/AuthRepository';
-import createAlert from '../../../features/shared/hooks/createAlert';
-import AlertErrors from '../../../features/shared/molecules/AlertErrors/AlertErrors';
 import { changeForgotPasswordAction } from './handler';
 
 const IndexPage: Component = () =>
 {
     const authRepository = new AuthRepository();
-    const errorAlert = createAlert();
     const [ searchParams ] = useSearchParams();
 
-    return ( <>
-        <AlertErrors errorData={errorAlert.errorData()} title="err_save" description="err_process_password"/>
+    return (
         <ChangeForgotPassword
             confirmationToken={searchParams.token}
-            changeForgotPasswordAction={changeForgotPasswordAction( { authRepository } )}
+            onSubmit={changeForgotPasswordAction( { authRepository } )}
         />
-    </>
     );
 };
 
