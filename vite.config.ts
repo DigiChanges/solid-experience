d26af17c4124df50d4e446ea2c25ec8a19bbf904
julian-saez <juliansaez.projects@gmail.com>
2022-11-15 10:14:31 -0300

    feat: NRS-45 change vite config and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import solid from 'solid-start/vite';
-import legacy from '@vitejs/plugin-legacy';
-import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
     plugins: [solid({ ssr: false })],
