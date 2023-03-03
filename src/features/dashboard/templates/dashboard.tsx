d22edf54bea758b63d65b64c37574f7822511b08
MatheitsonZilla <mteixeira@digichanges.com>
2023-03-03 18:04:57 -0300

    refactor: change wizard default color

--- src/features/dashboard/templates/dashboard.tsx
+++ src/features/dashboard/templates/dashboard.tsx
@@ -2,6 +2,7 @@ import { Component } from 'solid-js';
 import Card from '../../shared/molecules/Card/Card';
 import CardContent from '../../shared/molecules/CardContent/CardContent';
 import styles from './Dashboard.module.css';
+import StepBar from "../../wizard/stepBar";
 
 const Dashboard: Component = () =>
 {
@@ -11,6 +12,7 @@ const Dashboard: Component = () =>
                 <CardContent class={styles.content}>
                     <p>Dashboard</p>
                     <p>esto es template dashboard</p>
+                    <StepBar stepsQuantity={4} actualStep={2}></StepBar>
                 </CardContent>
             </Card>
         </section>
--- src/features/wizard/stepBar.tsx
+++ src/features/wizard/stepBar.tsx
@@ -24,11 +24,11 @@ const StepBar: Component<stepBarProps> = ( props ) =>
             <For each={numbers}>{
                 ( number, index ) =>
                     <>
-                        <div style={{ 'background-color': props.actualStep >= index() + 1 ? `${props.numbersContainerEndColor || '#41B400'}` : `${props.numbersContainerStartColor || '#f5f5f5'}`, 'transition': 'background-color .5s, color .5s', 'transition-delay': props.effectTotalTime || '.5s', 'width': props.numbersWidth || '26px', 'height': props.numbersHeight || '26px', 'color': props.actualStep >= index() + 1 ? props.numberEndColor || 'white' : props.numberStartColor || 'black' }} class={`rounded-[50%] z-1 flex justify-center items-center ${styles.notGrow}`}>
+                        <div style={{ 'background-color': props.actualStep >= index() + 1 ? `${props.numbersContainerEndColor || '#00c2d7'}` : `${props.numbersContainerStartColor || '#f5f5f5'}`, 'transition': 'background-color .5s, color .5s', 'transition-delay': props.effectTotalTime || '.5s', 'width': props.numbersWidth || '26px', 'height': props.numbersHeight || '26px', 'color': props.actualStep >= index() + 1 ? props.numberEndColor || 'white' : props.numberStartColor || 'black' }} class={`rounded-[50%] z-1 flex justify-center items-center ${styles.notGrow}`}>
                             {index() + 1}
                         </div>
                         {index() < props.stepsQuantity - 1 &&
-                        <div style={{ 'background': `linear-gradient(to right, ${props.lineEndColor || '#41B400'} 50%, ${props.lineStartColor || '#f5f5f5'} 50% ) 0px center/200%`, 'background-size': '200%', 'background-position': props.actualStep > index() + 1 ? '0' : '-100%', 'transition': `background-position ${props.effectTotalTime || '.5s'}`, 'transform': 'rotate(180deg)', 'height': props.lineHeight || '6px' }} class={`ml-[-1px] z-0 ${styles.grow}`} />
+                        <div style={{ 'background': `linear-gradient(to right, ${props.lineEndColor || '#00c2d7'} 50%, ${props.lineStartColor || '#f5f5f5'} 50% ) 0px center/200%`, 'background-size': '200%', 'background-position': props.actualStep > index() + 1 ? '0' : '-100%', 'transition': `background-position ${props.effectTotalTime || '.5s'}`, 'transform': 'rotate(180deg)', 'height': props.lineHeight || '6px' }} class={`ml-[-1px] z-0 ${styles.grow}`} />
                         }
                     </>
             }
