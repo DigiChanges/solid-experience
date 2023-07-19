3d95be55efbe265ff80a0510eb61ffa6835491c6
veronicadip <dipmveronica@gmail.com>
2023-07-19 13:14:48 -0300

    fix: changed conditionals and numbers displaying on screen

--- src/features/shared/molecules/Pagination/Pagination.tsx
+++ src/features/shared/molecules/Pagination/Pagination.tsx
@@ -9,16 +9,16 @@ interface PaginationProps {
 
 const Pagination: Component<PaginationProps> = ({count, eventHandler, currentPage}) => {
     return (
-        <>
-            <button class={`pagination-button-left${currentPage === 1 ? ` disabled-pagination` : ``}`} onClick={eventHandler}>{"<-"}</button>
+        <div class="pagination-component">
+            <button class={`pagination-button${currentPage === 1 ? ` disabled-pagination` : ``}`} disabled={currentPage === 1} onClick={eventHandler}>{"Previous"}</button>
             {[...Array(count)].map((_,i) => {
             if (i + 1 !== currentPage) {
                 return <button class="pagination-button" onClick={eventHandler}>{i + 1}</button>
          }
-         return <button class="selected-pagination" onClick={eventHandler}>{i + 1}</button>
+         return <button class="pagination-button selected-pagination" onClick={eventHandler}>{i + 1}</button>
         })}
-            <button class={`pagination-button-right${currentPage === count ? ` disabled-pagination` : ``}`} onClick={eventHandler}>{"->"}</button>
-        </>
+            <button class={`pagination-button${currentPage === count ? ` disabled-pagination` : ``}`} disabled={currentPage === count} onClick={eventHandler}>{"Next"}</button>
+        </div>
     )
 }
 
