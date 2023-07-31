2973d15e35b7951b23ac983601ce0ddd578a7d13
nrusso <natanaelrusso18@hotmail.com>
2023-07-31 10:05:56 -0300

    fix: remove permissions logic

--- src/atoms/Icons/Stroke/IconBurger.tsx
+++ src/atoms/Icons/Stroke/IconBurger.tsx
@@ -1,9 +1,11 @@
 import styles from './iconBurguer.module.css';
 import { Component } from 'solid-js';
 
-interface IconBurgerProps {
+interface IconBurgerProps
+{
     isOpened: boolean;
 }
+
 const IconBurger: Component<IconBurgerProps> = (props) => (
     <button class={styles.lineContainer} style={{ outline: 'none', border: 'none' }}>
         <span class={props.isOpened ? styles.line : `${styles.line} ${styles.line1Closed}`} />
