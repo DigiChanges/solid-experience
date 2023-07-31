a2a5541c3c756bbbe6661de915b435d1b4390c68
nrusso <natanaelrusso18@hotmail.com>
2023-07-30 22:07:55 -0300

    fix: login redirection

--- src/features/auth/login/templates/LoginTemplate.tsx
+++ src/features/auth/login/templates/LoginTemplate.tsx
@@ -22,7 +22,7 @@ const LoginTemplate: Component = () =>
     const { setError } = errorAlert;
     const { translate: t } = useTranslation();
 
-    const handleSuccess = () => () =>
+    const handleSuccess = () =>
     {
         navigate('/dashboard', { replace: true });
     };
@@ -57,12 +57,12 @@ const LoginTemplate: Component = () =>
                         </div>
 
                         <Show when={!getShowRecoverPassword()}
-                            fallback={
+                            fallback={(
                                 <ForgotPasswordForm
                                     onClick={togglePasswordRecovery({ setShowRecoverPassword, getShowRecoverPassword })}
                                     onSubmit={createForgotPasswordAction({ errorAlert, navigate, t })}
                                 />
-                            }
+                            )}
                         >
                             <div class={styles.register}>
                                 <p class={'text-neutral-50'}>{t('a_do_not_have_account')}</p>
@@ -74,8 +74,8 @@ const LoginTemplate: Component = () =>
                             <LoginForm
                                 onClick={togglePasswordRecovery({ setShowRecoverPassword, getShowRecoverPassword })}
                                 onSubmit={handleLoginFormSubmit()}
-                                onSuccess={handleSuccess()}
                                 onError={handleError()}
+                                onSuccess={handleSuccess}
                             />
                         </Show>
                     </div>
--- src/features/shared/constants/index.ts
+++ src/features/shared/constants/index.ts
@@ -5,15 +5,15 @@ export const INIT_STATE = {
     modalData: null
 };
 
-export const LOGIN_PAGE_PATH = '/login';
-export const CHANGE_FORGOT_PASSWORD_PAGE_PATH = '/change-forgot-password';
+export const LOGIN_PAGE_PATH = '/auth/login';
+export const CHANGE_FORGOT_PASSWORD_PAGE_PATH = '/auth/change-forgot-password';
 
 // these redirect paths to home page / when user is signed in
 export const REDIRECT_SIGNED_IN_USERS_PAGES = [
     LOGIN_PAGE_PATH,
     CHANGE_FORGOT_PASSWORD_PAGE_PATH,
     '/register',
-    '/change-password-success',
+    '/auth/change-password-success',
     '/email-sent-successfully',
     '/verify-account-success',
     '/verify-your-account'
